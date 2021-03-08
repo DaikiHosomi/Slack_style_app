e76ca6c7f0232711b3d3bdc84e11692b0404c530
DaikiHosomi <daikho2021@gmail.com>
2021-03-08 18:03:05 +0900

    チャットフォームコンポーネントの作成

---
+++ components/ChatForm.vue
@@ -0,0 +1,23 @@
+<template>
+    
+        <div class="input-container">
+            <textarea></textarea>
+        </div>
+    
+</template>
+
+<style scoped>
+.input-layout {
+    height: 10%;
+}
+
+.input-container {
+    padding: 10px;
+    height: 100%;
+}
+
+textarea {
+    width: 100%;
+    height: 100%;
+}
+</style>
