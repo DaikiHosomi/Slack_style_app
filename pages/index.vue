543c889882522f2c5ee5704402251153dac9eb43
DaikiHosomi <daikho2021@gmail.com>
2021-03-10 23:22:55 +0900

    firebaseのDBをimport

--- pages/index.vue
+++ pages/index.vue
@@ -12,11 +12,25 @@
 <script>
 import Messages from '~/components/Messages.vue'
 import ChatForm from '~/components/ChatForm.vue'
+import { db } from '~/plugins/firebase'
 
 export default {
   components: {
     Messages,
     ChatForm
+  },
+  mounted() {
+    // db.collection('channels').doc('aqOEpyCadAordcIKXoCY').get()
+    // .then((doc) => {
+    //   console.log('doc.id: ', doc.id)
+    //   console.log('doc.data(): ', doc.data())
+    // })
+    // db.collection('channels').get()
+    // .then((querySnapshot) => {
+    //   querySnapshot.forEach(function(doc) {
+    //     console.log(doc.id, " =>" , doc.data())
+    //   })
+    // })
   }
 }
 </script>
